f6c8940f2269dc06882d4d3d865c7f3e305b7a85
Sriniketh Sekar <148874325+SrinikethSekar@users.noreply.github.com>
2025-02-05 20:12:27 +0530

    Update bcdadd100.v

--- Verilog-Features/bcdadd100.v
+++ Verilog-Features/bcdadd100.v
@@ -1,1 +1,26 @@
-a
+module bcd_100adder (
+    input [399:0] a,  
+    input [399:0] b,  
+    input         cin,  
+    output        cout,  
+    output [399:0] sum  
+);
+
+    wire [99:0] carry;  
+
+    genvar i;
+    generate
+        for (i = 0; i < 100; i = i + 1) begin : bcd_adders
+            bcd_fadd adder (
+                .a(a[4*i +: 4]),  
+                .b(b[4*i +: 4]),  
+                .cin(i == 0 ? cin : carry[i-1]),  
+                .sum(sum[4*i +: 4]),  
+                .cout(carry[i])  
+            );
+        end
+    endgenerate
+
+    assign cout = carry[99];  
+
+endmodule
